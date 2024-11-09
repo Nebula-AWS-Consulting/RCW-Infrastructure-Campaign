4f0f99794b5b32722736894c963fde38acdb0c18
Emmanuel Urias <emmanuelurias60@icloud.com>
2024-11-08 19:11:18 -0800

    Optimized it

--- src/bin/cfn-deploy.sh
+++ src/bin/cfn-deploy.sh
@@ -1,4 +1,7 @@
 aws cloudformation deploy \
     --template-file ../infrastructure/arch-foundation.yaml \
     --stack-name RCW-Architecture-Foundation-Dev \
-    --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+    --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --change-set-name ChangeSet1 \
+    --parameter-overrides \
+        Environment=Dev
