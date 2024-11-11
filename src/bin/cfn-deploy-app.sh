0632e01138d26e5ce56d0ce2d31e34ae2e1c9bbb
Emmanuel Urias <emmanuelurias60@icloud.com>
2024-11-10 20:58:38 -0800

    Create app infrastructure

--- src/bin/cfn-deploy-app.sh
+++ src/bin/cfn-deploy-app.sh
@@ -2,6 +2,7 @@ aws cloudformation deploy \
     --template-file ../infrastructure/arch-app.yaml \
     --stack-name RCW-Architecture-App-Dev \
     --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
-    --change-set-name ChangeSet1 \
+    --no-execute-changeset \
+    --region us-west-1 \
     --parameter-overrides \
         Environment=Dev
