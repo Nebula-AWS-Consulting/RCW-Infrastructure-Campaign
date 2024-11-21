8f1f27a66705a5eb803b135880679d8109aa0f69
Emmanuel Urias <emmanuelurias60@icloud.com>
2024-11-21 11:25:53 -0800

    Locations page update

--- src/client-frontend/src/modules/pages/Location.tsx
+++ src/client-frontend/src/modules/pages/Location.tsx
@@ -1,27 +1,64 @@
-import React from 'react'
+import React from 'react';
 import AppAppBar from '../views/AppAppBar';
 import AppForm from '../views/AppForm';
-import { Box, Link, Typography } from '@mui/material';
+import { Box, Link } from '@mui/material';
 import AppFooter from '../views/AppFooter';
 import withRoot from '../withRoot';
+import EventCard from '../components/Event';
+import Typography from '../components/Typography';
 
 function Location() {
-    return (
+  return (
+    <React.Fragment>
+      <AppAppBar />
+      <AppForm>
         <React.Fragment>
-          <AppAppBar />
-          <AppForm>
-            <React.Fragment>
-              <Typography variant="h3" gutterBottom marked="center" align="center">
-                Location
-              </Typography>
-              <Box>
-
-              </Box>
-            </React.Fragment>
-          </AppForm>
-          <AppFooter />
+          <Typography variant="h3" gutterBottom marked='center' align="center">
+            Locations
+          </Typography>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            mt: '4rem',
+            gap: '20px'
+          }}>
+            <EventCard 
+                name='Sunday Service'
+                location='Cornerstone Park'
+                date='Sundays'
+                time='5PM'
+                size='large'
+            />
+            <EventCard 
+                name='Campus Devo'
+                location='UNLV'
+                date='Fridays'
+                time='7:30PM'
+                size='large'
+                description=''
+            />
+            <EventCard 
+                name='Bible Talk'
+                location='Contact Us'
+                date='Fridays'
+                time='7:30PM'
+                size='large'
+                description=''
+            />
+            <EventCard 
+                name='Mid-Week'
+                location='UNLV'
+                date='Wednesdays'
+                time='7:30PM'
+                size='large'
+                description='Alternates every other week so make sure to contact us for more info'
+            />
+          </Box>
         </React.Fragment>
-      );
+      </AppForm>
+      <AppFooter />
+    </React.Fragment>
+  );
 }
 
-export default withRoot(Location)
+export default withRoot(Location);
