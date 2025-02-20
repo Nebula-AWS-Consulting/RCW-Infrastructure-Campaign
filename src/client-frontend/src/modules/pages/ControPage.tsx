a3daa8776953a02674f57fd8e8181b915535ba1f
Emmanuel Urias <emmanuelurias60@icloud.com>
2025-02-19 23:03:39 -0800

    Add multi lang to the contro page

--- src/client-frontend/src/modules/pages/ControPage.tsx
+++ src/client-frontend/src/modules/pages/ControPage.tsx
@@ -15,6 +15,7 @@ import { SERVER } from "../../App";
 import FormFeedback from "../form/FormFeedback";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
+import { selectLanguage } from "../ducks/userSlice";
 
 const OneTimePaymentComponent = ({
     donationAmountRef,
@@ -262,7 +263,7 @@ const ControPage = () => {
   const [showThankYouBanner, setShowThankYouBanner] = useState(false);
   const user = useSelector((state: RootState) => state.userAuthAndInfo.user ?? { user_name: null, email: null });
   const token = useSelector((state: RootState) => state.userAuthAndInfo.token ?? { user_id: null, id_token: null, access_token: null, refresh_token: null });
-  console.log(user)
+  const language = useSelector(selectLanguage);
 
   const initialOptions = {
       clientId: "AfYXn-9V-9VfmWexdtRa8Q6ZYBQ4eU8cW8J01x4_BfCMuEuHN3kOc1eP9V-VYjYcqktNR06NuSr-UqT9",
@@ -283,14 +284,24 @@ const ControPage = () => {
         {showThankYouBanner && (
         <FormFeedback isDefault sx={{ mb: '-2rem', mt: 4, textAlign: 'center', width: '60%', justifySelf: 'center', borderRadius: 2 }}>
             <Typography variant="h6" color="white">
-            Thank you for donating!
+            {language === 'en-US'? 'Thank you for donating!' : language === 'fr-FR' ? `Merci pour votre don !` : language === 'es-MX' ? '¡Gracias por donar!' : ''}
             </Typography>
         </FormFeedback>
         )}
       <AppForm>
-        <Typography variant="h5" align="center" mb={2} justifySelf={'center'}>Donate to</Typography>
+        <Typography variant="h5" align="center" mb={2} justifySelf={'center'}>
+        {language === 'en-US'? `Donate to` 
+              : language === 'fr-FR' ? `Faire un don à` 
+              : language === 'es-MX' ? 'Donar a' 
+              : ''}
+        </Typography>
         <Typography variant="h4" gutterBottom marked="center" align="center">{`Restored Church ${import.meta.env.VITE_CHURCH_CITY}`}</Typography>
-        <Typography variant="h5" align="center" my={3} width={'80%'} justifySelf={'center'}>Weekly Contribution</Typography>
+        <Typography variant="h5" align="center" my={3} width={'80%'} justifySelf={'center'}>
+        {language === 'en-US'? `Weekly Contribution` 
+              : language === 'fr-FR' ? `Cotisation Hebdomadaire` 
+              : language === 'es-MX' ? 'Contribución Semanal' 
+              : ''}
+        </Typography>
 
         {/* Donation Amount Input */}
         <Box sx={{
@@ -301,7 +312,7 @@ const ControPage = () => {
             width: '100%'
         }}>
             <TextField
-                label="Donation Amount"
+                label={`${language === 'en-US'? `Donation Amount` : language === 'fr-FR' ? `Montant du don` : language === 'es-MX' ? 'Monto de la donación' : ''}`}
                 type="number"
                 value={donationAmount}
                 onChange={(e) => {
@@ -316,7 +327,7 @@ const ControPage = () => {
                     </InputAdornment>
                     ),
                 }}
-                helperText={Number(donationAmount) <= 0 && "Please enter a valid amount."}
+                helperText={Number(donationAmount) <= 0 && `${language === 'en-US'? `Please enter a valid amount.` : language === 'fr-FR' ? `Veuillez saisir un montant valide.` : language === 'es-MX' ? 'Por favor ingrese una cantidad válida.' : ''}`}
                 error={Number(donationAmount) <= 0}
                 />
         </Box>
@@ -355,7 +366,7 @@ const ControPage = () => {
             onClick={() => setPaymentType("one-time")}
             >
             <Typography variant="h5">
-                One-Time
+            {language === 'en-US'? `One-Time` : language === 'fr-FR' ? `Une Fois` : language === 'es-MX' ? 'Una Sola Vez' : ''}
             </Typography>
         </Box>
         <Box
@@ -376,7 +387,7 @@ const ControPage = () => {
             onClick={() => setPaymentType("subscription")}
             >
             <Typography variant="h5">
-                Subscription
+            {language === 'en-US'? `Subscription` : language === 'fr-FR' ? `Abonnement` : language === 'es-MX' ? 'Suscripción' : ''}
             </Typography>
         </Box>
         </Box>
